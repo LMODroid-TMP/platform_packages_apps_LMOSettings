aa6d51e263c262f76565e68243988bd275ffa23f
Dhina17 <dhinalogu@gmail.com>
2023-10-02 15:04:10 +0530

    Settings: Add config to handle LMO features availability
    Enabled by default.
    
    Change-Id: Id1a008057c564af04aa904cb614ca733d441ade7

--- src/com/android/settings/libremobileos/statusbar/StatusBarSettings.java
+++ src/com/android/settings/libremobileos/statusbar/StatusBarSettings.java
@@ -5,6 +5,7 @@
  */
 package com.android.settings.libremobileos.statusbar;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -186,5 +187,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.status_bar_settings);
+            new BaseSearchIndexProvider(R.xml.status_bar_settings) {
+
+        @Override
+        protected boolean isPageSearchEnabled(Context context) {
+            // Enable page search only if LMO features are available.
+            return context.getResources()
+                    .getBoolean(R.bool.config_show_lmo_features_settings);
+        }
+
+    };
 }
