LMODroid-TMP/platform_packages_apps_Settings

Implement notificationlight settings
Based on https://github.com/LineageOS/android_packages_apps_LineageParts

[Updated to U by: ikeramat]
ikeramat: taken and adapted from: https://github.com/LineageOS/android_packages_apps_LineageParts/commit/18f2dec2479692fd9e2b97d54acdfddf47d6150d

Change-Id: I44457aa51377bc70a1fdb11dfa089a17732dfd85
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

src/com/android/settings/libremobileos/notificationlight/BatteryBrightnessPreference.java
@@ -1,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.libremobileos.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class BatteryBrightnessPreference extends BrightnessPreference {
+    private static final String TAG = "BatteryBrightnessPreference";
+
+    private final ContentResolver mResolver;
+
+    public BatteryBrightnessPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                Settings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                Settings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}

src/com/android/settings/libremobileos/notificationlight/BatteryBrightnessZenPreference.java
@@ -1,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.libremobileos.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class BatteryBrightnessZenPreference extends BrightnessPreference {
+    private static final String TAG = "BatteryBrightnessZenPreference";
+
+    private final ContentResolver mResolver;
+
+    public BatteryBrightnessZenPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                Settings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL_ZEN,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                Settings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL_ZEN,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}

src/com/android/settings/libremobileos/notificationlight/NotificationBrightnessPreference.java
@@ -1,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.libremobileos.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.util.AttributeSet;
+
+import android.provider.Settings;
+
+public class NotificationBrightnessPreference extends BrightnessPreference {
+    private static final String TAG = "NotificationBrightnessPreference";
+
+    private final ContentResolver mResolver;
+
+    public NotificationBrightnessPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                Settings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                Settings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}

LMODroid-TMP/platform_packages_apps_LMOSettings

src/com/android/settings/libremobileos/notificationlight/BatteryBrightnessPreference.java
@@ -1,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.libremobileos.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import com.libremobileos.providers.LMOSettings;
+
+public class BatteryBrightnessPreference extends BrightnessPreference {
+    private static final String TAG = "BatteryBrightnessPreference";
+
+    private final ContentResolver mResolver;
+
+    public BatteryBrightnessPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                LMOSettings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                LMOSettings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}

src/com/android/settings/libremobileos/notificationlight/BatteryBrightnessZenPreference.java
@@ -1,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.libremobileos.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import com.libremobileos.providers.LMOSettings;
+
+public class BatteryBrightnessZenPreference extends BrightnessPreference {
+    private static final String TAG = "BatteryBrightnessZenPreference";
+
+    private final ContentResolver mResolver;
+
+    public BatteryBrightnessZenPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                LMOSettings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL_ZEN,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                LMOSettings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL_ZEN,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}

src/com/android/settings/libremobileos/notificationlight/NotificationBrightnessPreference.java
@@ -1,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.libremobileos.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.util.AttributeSet;
+
+import com.libremobileos.providers.LMOSettings;
+import android.provider.Settings;
+
+public class NotificationBrightnessPreference extends BrightnessPreference {
+    private static final String TAG = "NotificationBrightnessPreference";
+
+    private final ContentResolver mResolver;
+
+    public NotificationBrightnessPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return Settings.System.getIntForUser(mResolver,
+                LMOSettings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        Settings.System.putIntForUser(mResolver,
+                LMOSettings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}