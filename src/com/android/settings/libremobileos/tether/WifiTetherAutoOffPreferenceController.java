979f88832bf13355de7a6bec7ae9e86356d34d30
Dhina17 <dhinalogu@gmail.com>
2024-03-23 12:20:42 +0000

    Revert "LMOSettings: convert hotspot timeout preference to a list preference"
    This reverts commit ca1c9444e1d413afcfdb3700d2466f84296f1c6f.
    
    Change-Id: I865ef2c5030bd878677deb3cf110e5e71c642926

--- src/com/android/settings/libremobileos/tether/WifiTetherAutoOffPreferenceController.java
+++ src/com/android/settings/libremobileos/tether/WifiTetherAutoOffPreferenceController.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2022 Project Kaleidoscope
- * Copyright (C) 2022 FlamingoOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,19 +19,23 @@ package com.android.settings.libremobileos.tether;
 import android.content.Context;
 import android.net.wifi.SoftApConfiguration;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
+import com.android.settings.libremobileos.tether.preference.WifiTetherAutoOffPreference;
+
 import com.android.settings.core.BasePreferenceController;
 
-public class WifiTetherAutoOffPreferenceController extends BasePreferenceController
-        implements Preference.OnPreferenceChangeListener {
+public class WifiTetherAutoOffPreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final int MAX_TIMEOUT_MINUTES = 90;
 
     private final WifiManager mWifiManager;
 
-    private ListPreference mPreference;
+    private WifiTetherAutoOffPreference mPreference;
 
     public WifiTetherAutoOffPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -48,42 +51,36 @@ public class WifiTetherAutoOffPreferenceController extends BasePreferenceControl
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mPreference = screen.findPreference(getPreferenceKey());
+        mPreference.setMax(MAX_TIMEOUT_MINUTES);
         updateDisplay();
     }
 
-    private long getAutoOffTimeout() {
-        final SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
+    private int getAutoOffTimeout() {
+        SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
         final boolean settingsOn = softApConfiguration.isAutoShutdownEnabled();
-        return settingsOn ? softApConfiguration.getShutdownTimeoutMillis() / 1000 : 0;
+        return settingsOn ? (int) (softApConfiguration.getShutdownTimeoutMillis() / 60 / 1000) : 0;
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final long timeout;
-        try {
-            timeout = Long.parseLong((String) newValue);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        final SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
-        final SoftApConfiguration newSoftApConfiguration =
+        int timeout = (int) newValue;
+        SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
+        SoftApConfiguration newSoftApConfiguration =
                 new SoftApConfiguration.Builder(softApConfiguration)
                         .setAutoShutdownEnabled(timeout > 0)
-                        .setShutdownTimeoutMillis(timeout * 1000)
+                        .setShutdownTimeoutMillis((long) timeout * 60 * 1000)
                         .build();
         return mWifiManager.setSoftApConfiguration(newSoftApConfiguration);
     }
 
     public void updateConfig(SoftApConfiguration.Builder builder) {
         if (builder == null) return;
-        final long timeout = getAutoOffTimeout();
+        final int timeout = getAutoOffTimeout();
         builder.setAutoShutdownEnabled(timeout > 0)
-                .setShutdownTimeoutMillis(timeout * 1000);
+                .setShutdownTimeoutMillis((long) timeout * 60 * 1000);
     }
 
     public void updateDisplay() {
-        if (mPreference != null) {
-            mPreference.setValue(String.valueOf(getAutoOffTimeout()));
-        }
+        mPreference.setValue(getAutoOffTimeout(), false);
     }
 }
